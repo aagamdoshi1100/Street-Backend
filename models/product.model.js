5cd772c53a4877975502506483c1941ec45f1035
Aagam Doshi <aagamdoshi1100@gmail.com>
2024-03-22 20:12:42 +0530

    refactor: Image public id dependency value

--- models/product.model.js
+++ models/product.model.js
@@ -2,11 +2,14 @@ const mongoose = require("mongoose");
 const productSchema = new mongoose.Schema(
   {
     Name: String,
-    Public_Id: String,
+    Public_Id: {
+      type: mongoose.Schema.Types.ObjectId,
+    },
     Price: Number,
     Rating: Number,
     Quantity: Number,
     Discount: Number,
+    Category: String,
     Return_Policy: Boolean,
     About: String,
   },
